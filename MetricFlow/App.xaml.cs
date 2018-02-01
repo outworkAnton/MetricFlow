ae3078eaea63174751ffd4e14e7d5de93449657b
outworkAnton <outwork.anton@gmail.com>
2018-02-01 16:23:35 +0300

    Added database delivery from Google Drive system. Added connection options

--- MetricFlow/App.xaml.cs
+++ MetricFlow/App.xaml.cs
@@ -1,17 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Data;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 using System.Windows;
+using Google.Apis.Download;
+using MetricFlow.Helpers;
+using static System.Configuration.ConfigurationManager;
 
 namespace MetricFlow
 {
+    /// <inheritdoc />
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        public static string DbConnectionString { get; set; }
+
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            try
+            {
+                var status = GoogleDriveHelper.SyncDatabase();
+
+                if (status.Status == DownloadStatus.NotStarted)
+                {
+                    throw new NetworkInformationException();
+                }
+
+                while (status.Status == DownloadStatus.Downloading)
+                {
+                    Debug.WriteLine("Downloading database file: " + status.BytesDownloaded);
+                }
+
+                if (status.Status == DownloadStatus.Failed)
+                {
+                    throw new FileLoadException(status.Exception?.Message);
+                }
+
+                Debug.WriteLine("Database file updated from server");
+                DbConnectionString = ConnectionStrings["MetricFlowDatabase"].ConnectionString;
+            }
+            catch (NetworkInformationException networkInformationException)
+            {
+                MessageBox.Show("Cannot connect to server\n" + networkInformationException.Message, "Cannot start application",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                throw;
+            }
+            catch (FileLoadException fileLoadException)
+            {
+                MessageBox.Show("Database file update or locate is failed\n" + fileLoadException.Message, "Cannot start application",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                throw;
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Cannot start application",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                Current.Shutdown();
+            }
+        }
     }
 }
---
+++ MetricFlow/Helpers/GoogleDriveHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Download;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+
+namespace MetricFlow.Helpers
+{
+    public static class GoogleDriveHelper
+    {
+        static readonly string[] Scopes = {DriveService.Scope.DriveReadonly};
+        private const string APPLICATION_NAME = "MetricFlow";
+
+        public static IDownloadProgress SyncDatabase()
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("client_id.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = Environment.GetFolderPath(
+                    Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/metric-flow.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    Environment.UserName,
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Debug.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            var service = new DriveService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = APPLICATION_NAME
+            });
+            Debug.WriteLine("Drive API service was created");
+
+            var databaseDirectory = Path.GetDirectoryName(Process.GetCurrentProcess()
+                                        .MainModule
+                                        .FileName) + "\\Database";
+            if (!Directory.Exists(databaseDirectory))
+            {
+                Debug.WriteLine("Directory of database not exist");
+                Directory.CreateDirectory(databaseDirectory);
+                Debug.WriteLine("Directory of database was created");
+            }
+            var databaseFileName = databaseDirectory + "\\Metric.Flow.Database";
+            var request = service.Files.Get("1OPaCtIMT89zY6gg5PZMQ3ygbIRn-gUJ6");
+            Debug.WriteLine("Gets database file from server");
+
+            using (var stream =
+                new FileStream(databaseFileName
+                    , FileMode.OpenOrCreate, FileAccess.ReadWrite,
+                    FileShare.ReadWrite))
+            {
+                return request.DownloadWithStatus(stream);
+            }
+        }
+    }
+}
