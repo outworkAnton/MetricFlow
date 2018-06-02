9a48b0b48ce6910b4baad65b677bffe4c491e141
outworkAnton <outwork.anton@gmail.com>
2018-06-02 16:52:49 +0300

    Adaptation continue

--- BusinessLogic/BusinessLogic.DI/BusinessLogicAutofacModule.cs
+++ BusinessLogic/BusinessLogic.DI/BusinessLogicAutofacModule.cs
@@ -1,4 +1,6 @@
-﻿using Autofac;
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
 using AutoMapper;
 using BusinessLogic.Contract;
 
@@ -26,8 +28,21 @@ namespace BusinessLogic.DI
             builder.RegisterType<StatisticService>()
                    .As<IStatisticService>()
                    .InstancePerLifetimeScope();
+
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            builder.RegisterAssemblyTypes(assemblies)
+                   .Where(t => typeof(Profile).IsAssignableFrom(t) && !t.IsAbstract && t.IsPublic)
+                   .As<Profile>();
+
+            builder.Register(c => new MapperConfiguration(cfg =>
+            {
+                foreach (var profile in c.Resolve<IEnumerable<Profile>>())
+                {
+                    cfg.AddProfile(profile);
+                }
+            })).AsSelf().SingleInstance();
             builder.Register(c => c.Resolve<MapperConfiguration>()
-                   .CreateMapper(c.Resolve))
+                                   .CreateMapper(c.Resolve))
                    .As<IMapper>()
                    .InstancePerLifetimeScope();
         }
