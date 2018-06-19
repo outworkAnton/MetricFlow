12083fd266fabc327e03f1af0c3d4220de348f1e
outworkAnton <outwork.anton@gmail.com>
2018-06-19 23:56:06 +0300

    Repositories logic fix & NuGet updates

--- BusinessLogic/BusinessLogic/RevisionService.cs
+++ BusinessLogic/BusinessLogic/RevisionService.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using BusinessLogic.Contract;
-using BL = BusinessLogic.Contract.Interfaces;
+using BLContractInterfaces = BusinessLogic.Contract.Interfaces;
 using DataAccess.Contract;
-using DataAccess.Contract.Models;
-using DA = DataAccess.Contract.Interfaces;
+using DAContractInterfaces = DataAccess.Contract.Interfaces;
+using DAContractModels = DataAccess.Contract.Models;
 
 namespace BusinessLogic
 {
@@ -15,30 +15,35 @@ namespace BusinessLogic
     {
         private readonly IMapper _mapper;
         private readonly IDatabaseRevisionRepository _repository;
-        private readonly IEnumerable<DA.IDatabaseRevision> _revisions;
+        private readonly IEnumerable<DAContractInterfaces.IDatabaseRevision> _revisions;
 
         public RevisionService(IDatabaseRevisionRepository repository, IMapper mapper)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
             _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-            _revisions = _repository.Get().GetAwaiter().GetResult().OfType<DA.IDatabaseRevision>();
+            _revisions = _repository.Get().GetAwaiter().GetResult().OfType<DAContractInterfaces.IDatabaseRevision>();
         }
 
-        public BL.IDatabaseRevision GetRevisionById(string id)
+        public BLContractInterfaces.IDatabaseRevision GetRevisionById(string id)
         {
-            return _mapper.Map<BL.IDatabaseRevision>(_revisions?.FirstOrDefault(revision => revision.Id == id));
+            return _mapper.Map<BLContractInterfaces.IDatabaseRevision>(_revisions?
+                .FirstOrDefault(revision => revision.Id == id));
         }
 
         public async Task DownloadLatestDatabaseRevision()
         {
-            var latestLocalRevision = _revisions?.OrderByDescending(revision => revision.Modified).FirstOrDefault();
-            if (GoogleDriveHelper.NeedDownload(_mapper.Map<BL.IDatabaseRevision>(latestLocalRevision)))
+            var latestLocalRevision = _revisions?
+                                      .OrderByDescending(revision => revision.Modified)
+                                      .FirstOrDefault();
+            if (GoogleDriveHelper.NeedDownload(_mapper.Map<BLContractInterfaces.IDatabaseRevision>(latestLocalRevision)))
             {
-                var latestRemoteRevision = await GoogleDriveHelper.GetLatestRemoteRevision().ConfigureAwait(false);
+                var latestRemoteRevision = await GoogleDriveHelper
+                                                 .GetLatestRemoteRevision()
+                                                 .ConfigureAwait(false);
                 if (latestRemoteRevision != null)
                 {
-                    var convertedModel = _mapper.Map<DatabaseRevision>(latestRemoteRevision);
-                    await _repository.Create(convertedModel).ConfigureAwait(false);
+                    await _repository.Create(_mapper.Map<DAContractModels.DatabaseRevision>(latestRemoteRevision))
+                                     .ConfigureAwait(false);
                 }
             }
         }
--- DataAccess/DataAccess/DataAccessRepository.cs
+++ DataAccess/DataAccess/DataAccessRepository.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 using AutoMapper;
 using DataAccess.Contract;
 using Microsoft.EntityFrameworkCore;
-using ContractInterfaces = DataAccess.Contract.Interfaces;
-using ContractModels = DataAccess.Contract.Models;
-using BaseModels = DataAccess.Models;
+using DAContractInterfaces = DataAccess.Contract.Interfaces;
+using DAContractModels = DataAccess.Contract.Models;
+using DABaseModels = DataAccess.Models;
 
 namespace DataAccess
 {
@@ -24,37 +24,37 @@ namespace DataAccess
 
         public virtual async Task<IEnumerable> Get()
         {
-            if (typeof(ContractInterfaces.ILocation).IsAssignableFrom(typeof(T)))
+            if (typeof(DAContractInterfaces.ILocation).IsAssignableFrom(typeof(T)))
             {
                 await _context.Locations.LoadAsync().ConfigureAwait(false);
                 return await _context.Locations.ToListAsync().ConfigureAwait(false);
             }
 
-            if (typeof(ContractInterfaces.IService).IsAssignableFrom(typeof(T)))
+            if (typeof(DAContractInterfaces.IService).IsAssignableFrom(typeof(T)))
             {
                 await _context.Services.LoadAsync().ConfigureAwait(false);
                 return await _context.Services.ToListAsync().ConfigureAwait(false) as IEnumerable<T>;
             }
 
-            if (typeof(ContractInterfaces.IMetric).IsAssignableFrom(typeof(T)))
+            if (typeof(DAContractInterfaces.IMetric).IsAssignableFrom(typeof(T)))
             {
                 await _context.Metrics.LoadAsync().ConfigureAwait(false);
                 return await _context.Metrics.ToListAsync().ConfigureAwait(false) as IEnumerable<T>;
             }
 
-            if (typeof(ContractInterfaces.IFormula).IsAssignableFrom(typeof(T)))
+            if (typeof(DAContractInterfaces.IFormula).IsAssignableFrom(typeof(T)))
             {
                 await _context.Formulas.LoadAsync().ConfigureAwait(false);
                 return await _context.Formulas.ToListAsync().ConfigureAwait(false) as IEnumerable<T>;
             }
 
-            if (typeof(ContractInterfaces.IStatistic).IsAssignableFrom(typeof(T)))
+            if (typeof(DAContractInterfaces.IStatistic).IsAssignableFrom(typeof(T)))
             {
                 await _context.Statistics.LoadAsync().ConfigureAwait(false);
                 return await _context.Statistics.ToListAsync().ConfigureAwait(false) as IEnumerable<T>;
             }
 
-            if (typeof(ContractInterfaces.IDatabaseRevision).IsAssignableFrom(typeof(T)))
+            if (typeof(DAContractInterfaces.IDatabaseRevision).IsAssignableFrom(typeof(T)))
             {
                 await _context.Revisions.LoadAsync().ConfigureAwait(false);
                 return await _context.Revisions.ToListAsync().ConfigureAwait(false);
@@ -65,29 +65,34 @@ namespace DataAccess
 
         public virtual async Task Update(T item)
         {
-//            switch (typeof(T))
-//            {
-//                case ILocation _:
-//                    _context.Locations.Update(item as Location);
-//                    break;
-//                case IService _:
-//                    _context.Services.Update(item as Service);
-//                    break;
-//                case IMetric _:
-//                    _context.Metrics.Update(item as Metric);
-//                    break;
-//                case IFormula _:
-//                    _context.Formulas.Update(item as Formula);
-//                    break;
-//                case IStatistic _:
-//                    _context.Statistics.Update(item as Statistic);
-//                    break;
-//                case IDatabaseRevision _:
-//                    _context.Revisions.Update(item as DatabaseRevision);
-//                    break;
-//                default:
-//                    return;
-//            }
+            if (typeof(DAContractInterfaces.ILocation).IsAssignableFrom(typeof(T)))
+            {
+                _context.Locations.Update(_mapper.Map<DABaseModels.Location>(item));
+            }
+            else if (typeof(DAContractInterfaces.IService).IsAssignableFrom(typeof(T)))
+            {
+                _context.Services.Update(_mapper.Map<DABaseModels.Service>(item));
+            }
+            else if (typeof(DAContractInterfaces.IMetric).IsAssignableFrom(typeof(T)))
+            {
+                _context.Metrics.Update(_mapper.Map<DABaseModels.Metric>(item));
+            }
+            else if (typeof(DAContractInterfaces.IFormula).IsAssignableFrom(typeof(T)))
+            {
+                _context.Formulas.Update(_mapper.Map<DABaseModels.Formula>(item));
+            }
+            else if (typeof(DAContractInterfaces.IStatistic).IsAssignableFrom(typeof(T)))
+            {
+                _context.Statistics.Update(_mapper.Map<DABaseModels.Statistic>(item));
+            }
+            else if (typeof(DAContractInterfaces.IDatabaseRevision).IsAssignableFrom(typeof(T)))
+            {
+                _context.Revisions.Update(_mapper.Map<DABaseModels.DatabaseRevision>(item));
+            }
+            else
+            {
+                return;
+            }
 
             await _context.SaveChangesAsync().ConfigureAwait(false);
             OnDatabaseChange();
@@ -95,29 +100,34 @@ namespace DataAccess
 
         public virtual async Task Delete(T item)
         {
-//            switch (typeof(T))
-//            {
-//                case ILocation _:
-//                    _context.Locations.Remove(item as Location);
-//                    break;
-//                case IService _:
-//                    _context.Services.Remove(item as Service);
-//                    break;
-//                case IMetric _:
-//                    _context.Metrics.Remove(item as Metric);
-//                    break;
-//                case IFormula _:
-//                    _context.Formulas.Remove(item as Formula);
-//                    break;
-//                case IStatistic _:
-//                    _context.Statistics.Remove(item as Statistic);
-//                    break;
-//                case IDatabaseRevision _:
-//                    _context.Revisions.Remove(item as DatabaseRevision);
-//                    break;
-//                default:
-//                    return;
-//            }
+            if (typeof(DAContractInterfaces.ILocation).IsAssignableFrom(typeof(T)))
+            {
+                _context.Locations.Remove(_mapper.Map<DABaseModels.Location>(item));
+            }
+            else if (typeof(DAContractInterfaces.IService).IsAssignableFrom(typeof(T)))
+            {
+                _context.Services.Remove(_mapper.Map<DABaseModels.Service>(item));
+            }
+            else if (typeof(DAContractInterfaces.IMetric).IsAssignableFrom(typeof(T)))
+            {
+                _context.Metrics.Remove(_mapper.Map<DABaseModels.Metric>(item));
+            }
+            else if (typeof(DAContractInterfaces.IFormula).IsAssignableFrom(typeof(T)))
+            {
+                _context.Formulas.Remove(_mapper.Map<DABaseModels.Formula>(item));
+            }
+            else if (typeof(DAContractInterfaces.IStatistic).IsAssignableFrom(typeof(T)))
+            {
+                _context.Statistics.Remove(_mapper.Map<DABaseModels.Statistic>(item));
+            }
+            else if (typeof(DAContractInterfaces.IDatabaseRevision).IsAssignableFrom(typeof(T)))
+            {
+                _context.Revisions.Remove(_mapper.Map<DABaseModels.DatabaseRevision>(item));
+            }
+            else
+            {
+                return;
+            }
 
             await _context.SaveChangesAsync().ConfigureAwait(false);
             OnDatabaseChange();
@@ -125,37 +135,40 @@ namespace DataAccess
 
         public virtual async Task<T> Create(T item)
         {
-            switch (item)
+            if (typeof(DAContractInterfaces.ILocation).IsAssignableFrom(typeof(T)))
+                await _context.Locations.AddAsync(_mapper.Map<DABaseModels.Location>(item))
+                              .ConfigureAwait(false);
+            else if (typeof(DAContractInterfaces.IService).IsAssignableFrom(typeof(T)))
             {
-                case ContractModels.Location _:
-                    await _context.Locations.AddAsync(item as BaseModels.Location).ConfigureAwait(false);
-                    await _context.SaveChangesAsync().ConfigureAwait(false);
-                    break;
-                case ContractModels.Service _:
-                    await _context.Services.AddAsync(item as BaseModels.Service).ConfigureAwait(false);
-                    await _context.SaveChangesAsync().ConfigureAwait(false);
-                    break;
-                case ContractModels.Metric _:
-                    await _context.Metrics.AddAsync(item as BaseModels.Metric).ConfigureAwait(false);
-                    await _context.SaveChangesAsync().ConfigureAwait(false);
-                    break;
-                case ContractModels.Formula _:
-                    await _context.Formulas.AddAsync(item as BaseModels.Formula).ConfigureAwait(false);
-                    await _context.SaveChangesAsync().ConfigureAwait(false);
-                    break;
-                case ContractModels.Statistic _:
-                    await _context.Statistics.AddAsync(item as BaseModels.Statistic).ConfigureAwait(false);
-                    await _context.SaveChangesAsync().ConfigureAwait(false);
-                    break;
-                case ContractModels.DatabaseRevision _:
-                    await _context.Revisions.AddAsync(_mapper.Map<BaseModels.DatabaseRevision>(item)).ConfigureAwait(false);
-                    await _context.SaveChangesAsync().ConfigureAwait(false);
-//                    createdItem = await _context.DatabaseRevisions.FindAsync((item as DatabaseRevision).Id)
-//                                                .ConfigureAwait(false) as T;
-                    break;
-                default:
-                    return null;
+                await _context.Services.AddAsync(_mapper.Map<DABaseModels.Service>(item))
+                              .ConfigureAwait(false);
             }
+            else if (typeof(DAContractInterfaces.IMetric).IsAssignableFrom(typeof(T)))
+            {
+                await _context.Metrics.AddAsync(_mapper.Map<DABaseModels.Metric>(item))
+                              .ConfigureAwait(false);
+            }
+            else if (typeof(DAContractInterfaces.IFormula).IsAssignableFrom(typeof(T)))
+            {
+                await _context.Formulas.AddAsync(_mapper.Map<DABaseModels.Formula>(item))
+                              .ConfigureAwait(false);
+            }
+            else if (typeof(DAContractInterfaces.IStatistic).IsAssignableFrom(typeof(T)))
+            {
+                await _context.Statistics.AddAsync(_mapper.Map<DABaseModels.Statistic>(item))
+                              .ConfigureAwait(false);
+            }
+            else if (typeof(DAContractInterfaces.IDatabaseRevision).IsAssignableFrom(typeof(T)))
+            {
+                await _context.Revisions.AddAsync(_mapper.Map<DABaseModels.DatabaseRevision>(item))
+                              .ConfigureAwait(false);
+            }
+            else
+            {
+                return null;
+            }
+
+            await _context.SaveChangesAsync().ConfigureAwait(false);
 
             OnDatabaseChange();
             var items = await Get().ConfigureAwait(false);
