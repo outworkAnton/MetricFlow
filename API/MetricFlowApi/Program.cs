5b15a444955ef5aaad190642d713382304bc3d5d
Anton Mazurov <outwork.anton@gmail.com>
2018-06-27 12:43:45 +0300

    Migrate to NET.Core platform Fix tests project Add Api project Remove UI project

---
+++ API/MetricFlowApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MetricFlowApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
