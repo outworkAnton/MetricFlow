8045fe8088237a7517adb60cfcca8d22cbf84570
Anton Mazurov <outwork.anton@gmail.com>
2018-10-16 13:39:45 +0300

    Clean solution usings

--- API/MetricFlow.WebApi/Startup.cs
+++ API/MetricFlow.WebApi/Startup.cs
@@ -1,26 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
-using AutoMapper;
-using BusinessLogic;
-using BusinessLogic.Contract;
-using BusinessLogic.Contract.Interfaces;
 using BusinessLogic.DI;
-using DataAccess;
-using DataAccess.Contract;
 using DataAccess.DI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace MetricFlow.WebApi
 {
